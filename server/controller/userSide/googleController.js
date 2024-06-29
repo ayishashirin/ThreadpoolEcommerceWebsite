8ad7db96495c6162397b425dac150e3b5b62ddf6
Aysha Sherin Vp <ayshasherinvp@gmail.com>
2024-06-29 17:46:03 +0530

    google ok

--- server/controller/userSide/googleController.js
+++ server/controller/userSide/googleController.js
@@ -4,7 +4,7 @@ module.exports = {
       return res.redirect('/failure');
     }
     console.log(req.user);
-    res.redirect("userSide/userHome");
+    res.redirect("/");
   },
 
   failureGoogleLogin: (req, res) => {
